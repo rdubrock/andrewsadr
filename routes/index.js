14298b040032cf1b65b712e33ae738c0dd307496
Russell DuBrock <russell.dubrock@gmail.com>
2015-06-26 15:17:57 -0700

    email adjustments

--- routes/index.js
+++ routes/index.js
@@ -14,28 +14,19 @@ router.post('/mail', function(req, res, next) {
   var mailgun = new Mailgun({apiKey: api_key, domain: domain});
 
   var data = {
-  //Specify email data
     from: 'contact@andrewsadrservices.com',
-  //The email to contact
     to: 'andrewselaine@gmail.com',
-  //Subject and text data  
     subject: 'Enquiry through website from '+req.body.name,
     html: '<h4>Name: </h4><p>'+req.body.name+'</p><h4>Email: </h4><p>'+req.body.email+'</p><h4>Phone: </h4><p>'+req.body.phone+'</p><h4>Message: </h4><p>'+req.body.message+'</p>'
   }
-
-    //Invokes the method to send emails given the above data with the helper library
     mailgun.messages().send(data, function (err, body) {
-        //If there is an error, render the error page
         if (err) {
-            res.end('error');
             console.log("got an error: ", err);
+            res.end('error');
         }
-        //Else we can greet    and leave
         else {
-            //Here "submitted.jade" is the view file for this landing page 
-            //We pass the variable "email" from the url parameter in an object rendered by Jade
-            res.end();
             console.log(body);
+            res.end();
         }
     });
 
