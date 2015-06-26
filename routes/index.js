838bc689192138a00680a694f31a4eebd8229920
Russell DuBrock <russell.dubrock@gmail.com>
2015-06-25 22:32:22 -0700

    email partially working (not all from domains functional)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Mailgun = require('mailgun-js');
+var mailGunUserInfo = require('./mailgunlogininfo.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/mail', function(req, res, next) {
-  console.log(req.body);
+  var api_key = mailGunUserInfo.api_key;
+  var domain = mailGunUserInfo.domain;  
+  var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+  var data = {
+  //Specify email data
+    from: req.body.email,
+  //The email to contact
+    to: 'adubrock@comcast.net',
+  //Subject and text data  
+    subject: 'Contact from website',
+    text: req.body.message+' Phone: '+req.body.phone
+  }
+
+    //Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            res.end('error');
+            console.log("got an error: ", err);
+        }
+        //Else we can greet    and leave
+        else {
+            //Here "submitted.jade" is the view file for this landing page 
+            //We pass the variable "email" from the url parameter in an object rendered by Jade
+            res.end();
+            console.log(body);
+        }
+    });
+
   res.end();
 })
 
